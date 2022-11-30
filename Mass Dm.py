a84a68ea947c47f543abad88241a060ff7a89589
Rattlers <94129991+heygdrg@users.noreply.github.com>
2022-11-30 18:26:21 +0100

    Add files via upload

--- Mass Dm.py
+++ Mass Dm.py
@@ -118,6 +118,7 @@ def spam_guild():
                             
                     else:
                         print(Colorate.Horizontal(Colors.green_to_white,f'[+] Scraping user : {id}' , 1))
+                        id_list.append(f'{id}\n')
                         id_scrap = id_scrap + 1
                         os.system(f'Title - ZEHEF mass DM - Guild scrap : {guild_scrap}  Channel scrap : {channel_scrap}  ID scrap : {id_scrap}  DM create : {dm_create}')
 
@@ -125,7 +126,9 @@ def spam_guild():
                 except Exception as e:
                     print(Colorate.Horizontal(Colors.red_to_white, "[-] An error ocured while scraping user id ", 1))
 
+    
     for iid in id_list: 
+        print('dm')
         payload =  {'recipients': [iid]}
         headers = { 
             'Cookie': '__dcfduid=30b25b30bdb811eca9acdd9d360ada08',
@@ -133,30 +136,31 @@ def spam_guild():
             'Content-Type': 'application/json',
             'x-super-properties': 'eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiQ2hyb21lIiwiZGV2aWNlIjoiIiwic3lzdGVtX2xvY2FsZSI6ImZyLUZSIiwiYnJvd3Nlcl91c2VyX2FnZW50IjoiTW96aWxsYS81LjAgKFdpbmRvd3MgTlQgMTAuMDsgV2luNjQ7IHg2NCkgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzEwNy4wLjAuMCBTYWZhcmkvNTM3LjM2IiwiYnJvd3Nlcl92ZXJzaW9uIjoiMTA3LjAuMC4wIiwib3NfdmVyc2lvbiI6IjEwIiwicmVmZXJyZXIiOiJodHRwczovL2Rpc2NvcmQuY29tLyIsInJlZmVycmluZ19kb21haW4iOiJkaXNjb3JkLmNvbSIsInJlZmVycmVyX2N1cnJlbnQiOiJodHRwczovL2Rpc2NvcmQuY29tLyIsInJlZmVycmluZ19kb21haW5fY3VycmVudCI6ImRpc2NvcmQuY29tIiwicmVsZWFzZV9jaGFubmVsIjoic3RhYmxlIiwiY2xpZW50X2J1aWxkX251bWJlciI6MTYwNjQ1LCJjbGllbnRfZXZlbnRfc291cmNlIjpudWxsfQ=='
         }
-    response = requests.post("https://discord.com/api/v8/users/@me/channels", headers=headers, json=payload).json()                
-      
-    try: 
-        channel_id_message = response['id']
-                
-        print(Colorate.Horizontal(Colors.green_to_white,f'[+] Channel : {channel_id_message} create with : {iid}' , 1))
-        dm_create = dm_create + 1
-        os.system(f'Title - ZEHEF mass DM - Guild scrap : {guild_scrap}  Channel scrap : {channel_scrap}  ID scrap : {id_scrap}  DM create : {dm_create}')
-
-                
-                
-            
-        r = requests.post(f'https://discord.com/api/v9/channels/{channel_id_message}/messages',
-                data={"content": f'{content}'},
-                headers={'Authorization': token}).json()
         
-        r = requests.post(f'https://discord.com/api/v9/channels/{r["channel_id"]}/messages',
-                data={"content": f'{content}'},
-                headers={'Authorization': token})
+        response = requests.post("https://discord.com/api/v8/users/@me/channels", headers=headers, json=payload).json()                
+      
+        try: 
+            channel_id_message = response['id']
+                    
+            print(Colorate.Horizontal(Colors.green_to_white,f'[+] Channel : {channel_id_message} create with : {iid}' , 1))
+            dm_create = dm_create + 1
+            os.system(f'Title - ZEHEF mass DM - Guild scrap : {guild_scrap}  Channel scrap : {channel_scrap}  ID scrap : {id_scrap}  DM create : {dm_create}')
 
-        print(Colorate.Horizontal(Colors.green_to_white,f'[+] message sent to {r["author"]["username"]}' , 1))    
-    
-    except:
-        print(Colorate.Horizontal(Colors.red_to_white, f'[-] An error happened with {iid} {response["message"]}', 1))
+                    
+                    
+                
+            r = requests.post(f'https://discord.com/api/v9/channels/{channel_id_message}/messages',
+                    data={"content": f'{content}'},
+                    headers={'Authorization': token}).json()
+            
+            r = requests.post(f'https://discord.com/api/v9/channels/{r["channel_id"]}/messages',
+                    data={"content": f'{content}'},
+                    headers={'Authorization': token})
+
+            print(Colorate.Horizontal(Colors.green_to_white,f'[+] message sent to {r["author"]["username"]}' , 1))    
+        
+        except:
+            print(Colorate.Horizontal(Colors.red_to_white, f'[-] An error happened with {iid} ', 1))
 
 def main():
     
@@ -183,7 +187,7 @@ def main():
     content_1 = '[?] Enter the message you want to send : '
     content = input(Colorate.Vertical(Colors.blue_to_white, Center.XCenter(content_1)))
 
-    threading.Thread(target = spam_message(), args=(content, token)).start()
+    #threading.Thread(target = spam_message(), args=(content, token)).start()
     threading.Thread(target = spam_guild(), args=(content, token)).start()
 
 #print(r[0]['author']['username'])
